adfca55b8b8f5d4a51615dab2c19c84834e602df
gnidinger <gnidinger@gmail.com>
2023-10-18 15:34:28 +0900

    fix:topicA04

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .controllers.naver_crawling_controller import configure_routes
 from .services.kafka_topic01_service import kafka_topic_01
 from .services.kafka_topic02_service import kafka_topic_02
 from .services.kafka_topic03_service import kafka_topic_03
+from .services.kafka_topic04_service import kafka_topic_04
 from .services.kafka_topic11_service import kafka_topic_11
 from .services.kafka_topic21_service import kafka_topic_21
 from .services.kafka_topic31_service import kafka_topic_31
@@ -22,6 +23,9 @@ def create_app():
     kafka_thread.start()
 
     kafka_thread = threading.Thread(target=kafka_topic_03, args=(app,))
+    kafka_thread.start()
+
+    kafka_thread = threading.Thread(target=kafka_topic_04, args=(app,))
     kafka_thread.start()
 
     kafka_thread = threading.Thread(target=kafka_topic_11, args=(app,))
